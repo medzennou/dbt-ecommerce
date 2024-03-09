7d8df851afc7d19302410d3e90d65da24f824bd2
med_zennou <zennou1md@gmail.com>
2024-03-09 16:50:16 +0100

    created the order model

---
+++ ecommerce/models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+
+{{ config(materialized='table')}}
+
+WITH source AS (
+	SELECT * FROM {{source('thelook_ecommerce','orders')}}
+)
+
+
+SELECT order_id,
+user_id,
+
+created_at,
+returned_at,
+delivered_at,
+shipped_at,
+
+status,
+num_of_item AS num_items_ordered
+
+FROM source
+LIMIT 20
